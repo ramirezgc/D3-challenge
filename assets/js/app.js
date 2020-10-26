66a82a4686ea833b89ee8b7073db79c1a5444851
Gina Ramirez <67246653+ramirezgc@users.noreply.github.com>
2020-10-25 20:43:30 -0500

    Build axis

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,17 +22,36 @@ let chartGroup = svg.append("g")
 d3.csv("./assets/data/data.csv").then(function(censusData){
     console.log(censusData);
 
-    censusData.forEach(function(selectedData){
-        selectedData.poverty = +selectedData.poverty;
-        selectedData.healthcare = +selectedData.healthcare;
+    censusData.forEach(function(data){
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
     });
 
     let xLinearScale = d3.scaleLinear()
-        .domain([d3.min(censusData, selectedData => selectedData.poverty),
-        d3.max(censusData, selectedData.poverty)])
+        .domain([d3.min(censusData, d => d.poverty),
+        d3.max(censusData, d => d.poverty)])
         .range([0, chartWidth]);
 
-    let yLinearScale = d3.selectedData()
-        .domain([0,d3.max(censusData, selectedData => selectedData.healthcare)])
+    let yLinearScale = d3.scaleLinear()
+        .domain([0,d3.max(censusData, d => d.healthcare)])
         .range([chartHeight,0]);
+
+    let bottomAxis = d3.axisBottom(xLinearScale);
+    let leftAxis = d3.axisLeft(yLinearScale);
+
+    let drawLine = d3.line()
+        .x(selectedData => xLinearScale(selectedData.poverty))
+        .y(selectedData => yLinearScale(selectedData.healthcare));
+    
+    // X Axis
+    chartGroup.append("g")
+    .attr("transform", `translate(0, ${chartHeight})`)
+    .call(bottomAxis);
+    
+    // Y Axis
+    chartGroup.append("g")
+        .classed("axis", true)
+        .call(leftAxis)
+
+    
 });
