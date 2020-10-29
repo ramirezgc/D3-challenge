ac167449ad7c7c1ee91ac0a073025f3223e74466
Gina Ramirez <67246653+ramirezgc@users.noreply.github.com>
2020-10-28 21:07:18 -0500

    added scatter plot

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
-let svgWidth = 960;
-let svgHeight = 500;
+let svgWidth = 1200;
+let svgHeight = 600;
 
 let margin = {
-    top: 60,
+    top: 20,
     right: 60,
-    bottom: 60,
-    left: 60
+    bottom: 20,
+    left: 120
 }
 
 let chartWidth = svgWidth - margin.left - margin.right;
@@ -15,6 +15,7 @@ let svg = d3.select('#scatter')
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
+   
 
 let chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
@@ -28,8 +29,8 @@ d3.csv("./assets/data/data.csv").then(function(censusData){
     });
 
     let xLinearScale = d3.scaleLinear()
-        .domain([d3.min(censusData, d => d.poverty),
-        d3.max(censusData, d => d.poverty)])
+        .domain([d3.min(censusData, d => d.poverty)*0.9,
+        d3.max(censusData, d => d.poverty)*1.10])
         .range([0, chartWidth]);
 
     let yLinearScale = d3.scaleLinear()
@@ -43,16 +44,52 @@ d3.csv("./assets/data/data.csv").then(function(censusData){
         .x(selectedData => xLinearScale(selectedData.poverty))
         .y(selectedData => yLinearScale(selectedData.healthcare));
     
-    // X Axis
     chartGroup.append("g")
         .attr("transform", `translate(0, ${chartHeight})`)
         .call(bottomAxis);   
 
-    
-    // Y Axis
     chartGroup.append("g")
         .classed("axis", true)
         .call(leftAxis);
 
-    
+    chartGroup.selectAll("circle")
+        .data(censusData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", 12)
+        .attr("fill", "blue")
+        .attr("opacity", ".6");
+
+    chartGroup.selectAll("text.text-circles")
+        .data(censusData)
+        .enter()
+        .append("text")
+        .classed("text-circles",true)
+        .text(d => d.abbr)
+        .attr("x", d => xLinearScale(d.poverty))
+        .attr("y", d => yLinearScale(d.healthcare))
+        .attr("dy",5)
+        .attr("text-anchor","middle")
+        .attr("font-size","10px")
+        .attr("fill", "white");
+
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 30 - margin.left)
+        .attr("x", 0 - (chartHeight / 2))
+        .attr("dy", "1em")
+        .classed("aText", true)
+        .attr("fill", "white")
+        .text("Lacks Healthcare (%)");
+
+    chartGroup.append("text")
+        .attr("y", chartHeight + margin.bottom/2 - 10)
+        .attr("x", chartWidth / 2)
+        .attr("dy", "1em")
+        .classed("aText", true)
+        .attr("fill", "white")
+        .text("In Poverty (%)");
+
 });
